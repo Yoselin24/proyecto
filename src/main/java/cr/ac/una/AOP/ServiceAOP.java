72571b145f6d816239dcec27bcc90e4b434884e4
Luis-12 <alfred,va98@gmail.com>
2019-09-19 22:10:43 -0600

    aop y la ventana de aoplist

--- src/main/java/cr/ac/una/AOP/ServiceAOP.java
+++ src/main/java/cr/ac/una/AOP/ServiceAOP.java
@@ -1,13 +1,60 @@
 package cr.ac.una.AOP;
 
+import cr.ac.una.entities.AOP;
 import cr.ac.una.services.AOPService;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
 
 @Aspect
 public class ServiceAOP {
 
     @Autowired
     AOPService aopService;
+    private AOP aop=new AOP();
+
+    @Before("execution( * cr.ac.una.services.PersonaService.createPersona(..))")
+    public void creteLogPersona(JoinPoint joinPoint)
+    {
+        java.util.Date fecha = new Date();
+        aop.setFecha(fecha);
+        aop.setTransaccion("Persona Creada");
+        aop.setClase("Persona");
+        aopService.createAOP(aop);
+    }
+
+    @Before("execution( * cr.ac.una.services.PersonaService.deletePersona(..))")
+    public void deleteLogPersona(JoinPoint joinPoint)
+    {
+        java.util.Date fecha = new Date();
+        aop.setFecha(fecha);
+        aop.setTransaccion("Persona Eliminada");
+        aop.setClase("Persona");
+        aopService.createAOP(aop);
+    }
+
+    @Before("execution( * cr.ac.una.services.PersonaService.getAllPersonas(..))")
+    public void listLogPersona(JoinPoint joinPoint)
+    {
+        java.util.Date fecha = new Date();
+        aop.setFecha(fecha);
+        aop.setTransaccion("Listado Personas");
+        aop.setClase("Persona");
+        aopService.createAOP(aop);
+    }
+
+    @Before("execution( * cr.ac.una.beans.PersonaBean.update(..))")
+    public void updateLogPersona(JoinPoint joinPoint)
+    {
+        java.util.Date fecha = new Date();
+        aop.setFecha(fecha);
+        aop.setTransaccion("Persona Modificada");
+        aop.setClase("Persona");
+        aopService.createAOP(aop);
+    }
+
 
 }
---
+++ src/main/java/cr/ac/una/beans/AopBean.java
@@ -0,0 +1,55 @@
+package cr.ac.una.beans;
+
+import cr.ac.una.entities.AOP;
+import cr.ac.una.entities.Mocion;
+import cr.ac.una.entities.TipoMocion;
+import cr.ac.una.services.AOPService;
+import cr.ac.una.services.MocionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Component
+public class AopBean {
+
+    @Autowired
+    cr.ac.una.services.AOPService aopService;
+    private AOP aop=new AOP();
+    private List<AOP> aops;
+
+    @PostConstruct
+    public void init() {
+        aops = aopService.getAllAOP();
+    }
+
+    public AOP getAop() {
+        return aop;
+    }
+
+    public void setAop(AOP aop) {
+        this.aop = aop;
+    }
+
+    public List<AOP> getAops() {
+        return aops;
+    }
+
+    public void setAops(List<AOP> aops) {
+        this.aops = aops;
+    }
+
+    public void create() {
+        aopService.createAOP(aop);
+        aops = aopService.getAllAOP();
+        aop=null;
+    }
+
+    public void refrescaLog()
+    {
+        aops=aopService.getAllAOP();
+    }
+
+
+
+}
